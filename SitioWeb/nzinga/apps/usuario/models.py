97b8484c6d0ad7d55c96fdc2a933b8169f0b9be6
TheKafer <38733280+TheKafer@users.noreply.github.com>
2019-06-28 16:07:52 -0500

    Nzinga v0
    Primeros pasos

--- SitioWeb/nzinga/apps/usuario/models.py
+++ SitioWeb/nzinga/apps/usuario/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+# Modelos de los usuarios.
+
+#Modelo de la entidad
+class Entity(models.Model):
+	iduser = models.IntegerField(primary_key=True)
+
+
+#Modelo que contiene la información de la entidad
+
+class Info_Entity(models.Model):
+	name = models.CharField(max_length=100)
+	numberDirectors = models.IntegerField(default=0)
+	state = models.BooleanField(default=True)
+	entity = models.OneToOneField(Entity,null=True, blank=True, on_delete=models.CASCADE) 
+
+#Modelo del director
+class Director(models.Model):
+	iduser = models.IntegerField(primary_key=True)
+
+#Modelo que contiene la información del director.
+class Info_Director(models.Model):
+	names = models.CharField(max_length=200)
+	LastNames = models.CharField(max_length=200)
+	state = models.BooleanField(default=True)
+	director = models.OneToOneField(Director,null=True, blank=True, on_delete=models.CASCADE) 
+
+
+
